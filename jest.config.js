5f286d345e1eb7eb2a03870ba82a39415c637e57
Víctor de Lara Martínez <vdelara.afd@gmail.com>
2023-08-26 18:47:57 +0200

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['dist'],
+  resolver: 'jest-ts-webcompat-resolver',
+  moduleNameMapper: {
+    '\\.(css|scss)$': 'identity-obj-proxy',
+  },
+};
